c4919af6b93a5c8f84b30249b36acd71a4c66b91
Waldemar Mozoła <waldemar.mozola1984@gmail.com>
2020-12-25 20:43:59 +0100

    modyfy gitignore

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -23,18 +23,17 @@ public class TicTacToe {
         System.out.println();
         for (int i = 0; i < BOARDSIZE; i++) {
             for (int j = 0; j < BOARDSIZE; j++) {
-                if(j==BOARDSIZE-1){
-                    System.out.print(" "+ board[i][j] +" \n");
-                }
-                else{
-                    System.out.print(" "+ board[i][j] +" |");
+                if (j == BOARDSIZE - 1) {
+                    System.out.print(" " + board[i][j] + " \n");
+                } else {
+                    System.out.print(" " + board[i][j] + " |");
                 }
             }
 
-            if(i<BOARDSIZE-1){
+            if (i < BOARDSIZE - 1) {
                 System.out.print("---");
-                int k=1;
-                while(k<=BOARDSIZE-1){
+                int k = 1;
+                while (k <= BOARDSIZE - 1) {
                     System.out.print("+---");
                     k++;
                 }
@@ -56,13 +55,13 @@ public class TicTacToe {
 
         while (true) {
             Scanner scan = new Scanner(System.in);
-            try{
+            try {
                 int x = scan.nextInt();
                 int o = scan.nextInt();
 
                 if (board[x][o] != 'X' && board[x][o] != 'O') {
                     board[x][o] = currentPlayer;
-                } else{
+                } else {
                     System.out.println("this field is busy, try another");
                     continue;
                 }
@@ -74,60 +73,60 @@ public class TicTacToe {
                     currentPlayer = 'X';
                     System.out.println("give the X position: ");
                 }
-            } catch(ArrayIndexOutOfBoundsException e){
+            } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("field position is out of bounds, try another");
-            }  catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("insert number");
             }
 
             printBoard();
 
             //check winner for rows
-            if(checkWinnerRows() == 'X') {
-                System.out.println("The winner is X" );
+            if (checkWinnerRows() == 'X') {
+                System.out.println("The winner is X");
                 break;
-            } else if(checkWinnerRows() == 'O'){
+            } else if (checkWinnerRows() == 'O') {
                 System.out.println("The winner is O");
                 break;
             }
 
             //check winner for cols
-            if(checkWinnerCols() == 'X') {
-                System.out.println("The winner is X" );
+            if (checkWinnerCols() == 'X') {
+                System.out.println("The winner is X");
                 break;
-            } else if(checkWinnerCols() == 'O'){
+            } else if (checkWinnerCols() == 'O') {
                 System.out.println("The winner is O");
                 break;
             }
 
             //check winner for right diagonal
-            if(checkWinnerRightDiagonal() == 'X') {
-                System.out.println("The winner is X" );
+            if (checkWinnerRightDiagonal() == 'X') {
+                System.out.println("The winner is X");
                 break;
-            } else if(checkWinnerRightDiagonal() == 'O'){
+            } else if (checkWinnerRightDiagonal() == 'O') {
                 System.out.println("The winner is O");
                 break;
             }
 
             //check winner for left diagonal
-            if(checkWinnerLeftDiagonal() == 'X') {
-                System.out.println("The winner is X" );
+            if (checkWinnerLeftDiagonal() == 'X') {
+                System.out.println("The winner is X");
                 break;
-            } else if(checkWinnerLeftDiagonal() == 'O'){
+            } else if (checkWinnerLeftDiagonal() == 'O') {
                 System.out.println("The winner is O");
                 break;
             }
 
             //check for death-heat
             int counter = 0;
-            for(int i=0; i<BOARDSIZE; i++){
-                for(int j=0; j<BOARDSIZE; j++){
-                    if(board[i][j] == ' '){
+            for (int i = 0; i < BOARDSIZE; i++) {
+                for (int j = 0; j < BOARDSIZE; j++) {
+                    if (board[i][j] == ' ') {
                         counter++;
                     }
                 }
             }
-            if(counter == 0){
+            if (counter == 0) {
                 System.out.println("Death-heat");
                 break;
             }
@@ -135,22 +134,22 @@ public class TicTacToe {
     }
 
     //4 check winner for rows
-    public static char checkWinnerRows(){
+    public static char checkWinnerRows() {
 
-        for(int i=0; i<BOARDSIZE; i++){
+        for (int i = 0; i < BOARDSIZE; i++) {
             int counterX = 0;
             int counterO = 0;
-            for(int j=0; j<BOARDSIZE; j++){
-                if(board[i][j] == 'X'){
+            for (int j = 0; j < BOARDSIZE; j++) {
+                if (board[i][j] == 'X') {
                     counterX++;
-                }   else if(board[i][j] == 'O'){
+                } else if (board[i][j] == 'O') {
                     counterO++;
                 }
             }
-            if(counterX == BOARDSIZE){
+            if (counterX == BOARDSIZE) {
                 return 'X';
             }
-            if(counterO == BOARDSIZE){
+            if (counterO == BOARDSIZE) {
                 return 'O';
             }
         }
@@ -158,22 +157,22 @@ public class TicTacToe {
     }
 
     //4 check winner for cols
-    public static char checkWinnerCols(){
+    public static char checkWinnerCols() {
 
-        for(int i=0; i<BOARDSIZE; i++){
+        for (int i = 0; i < BOARDSIZE; i++) {
             int counterXCols = 0;
             int counterOCols = 0;
-            for(int j=0; j<BOARDSIZE; j++){
-                if(board[j][i] == 'X'){
+            for (int j = 0; j < BOARDSIZE; j++) {
+                if (board[j][i] == 'X') {
                     counterXCols++;
-                }   else if(board[j][i] == 'O'){
+                } else if (board[j][i] == 'O') {
                     counterOCols++;
                 }
             }
-            if(counterXCols == BOARDSIZE){
+            if (counterXCols == BOARDSIZE) {
                 return 'X';
             }
-            if(counterOCols == BOARDSIZE){
+            if (counterOCols == BOARDSIZE) {
                 return 'O';
             }
         }
@@ -181,20 +180,20 @@ public class TicTacToe {
     }
 
     //4 check winner for right diagonal
-    public static char checkWinnerRightDiagonal(){
+    public static char checkWinnerRightDiagonal() {
 
         int counterX = 0;
         int counterY = 0;
-        for(int i=0; i<BOARDSIZE; i++){
-            if(board[i][BOARDSIZE-1-i] == 'X'){
+        for (int i = 0; i < BOARDSIZE; i++) {
+            if (board[i][BOARDSIZE - 1 - i] == 'X') {
                 counterX++;
-            }   else if(board[i][BOARDSIZE-1-i] == 'O'){
+            } else if (board[i][BOARDSIZE - 1 - i] == 'O') {
                 counterY++;
             }
-            if(counterX == BOARDSIZE){
+            if (counterX == BOARDSIZE) {
                 return 'X';
             }
-            if(counterY == BOARDSIZE){
+            if (counterY == BOARDSIZE) {
                 return 'O';
             }
         }
@@ -202,27 +201,27 @@ public class TicTacToe {
     }
 
     //4 check winner for left diagonal
-    public static char checkWinnerLeftDiagonal(){
+    public static char checkWinnerLeftDiagonal() {
 
         int counterX = 0;
         int counterY = 0;
-        for(int i=0; i<BOARDSIZE; i++){
-            if(board[i][i] == 'X'){
+        for (int i = 0; i < BOARDSIZE; i++) {
+            if (board[i][i] == 'X') {
                 counterX++;
-            }   else if(board[i][i] == 'O'){
+            } else if (board[i][i] == 'O') {
                 counterY++;
             }
-            if(counterX == BOARDSIZE){
+            if (counterX == BOARDSIZE) {
                 return 'X';
             }
-            if(counterY == BOARDSIZE){
+            if (counterY == BOARDSIZE) {
                 return 'O';
             }
         }
         return '/';
     }
 
-    public static void howToPlay(){
+    public static void howToPlay() {
 
         System.out.println();
         System.out.println("HOW TP PLAY:");
@@ -236,18 +235,17 @@ public class TicTacToe {
 
         for (int i = 0; i < BOARDSIZE; i++) {
             for (int j = 0; j < BOARDSIZE; j++) {
-                if(j==BOARDSIZE-1){
-                    System.out.print(" "+ i + j +" \n");
-                }
-                else{
-                    System.out.print(" "+ i + j +" |");
+                if (j == BOARDSIZE - 1) {
+                    System.out.print(" " + i + j + " \n");
+                } else {
+                    System.out.print(" " + i + j + " |");
                 }
             }
 
-            if(i<BOARDSIZE-1){
+            if (i < BOARDSIZE - 1) {
                 System.out.print("----");
-                int k=1;
-                while(k<=BOARDSIZE-1){
+                int k = 1;
+                while (k <= BOARDSIZE - 1) {
                     System.out.print("+----");
                     k++;
                 }
